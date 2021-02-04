d90caaa0f1dd5d66a9ac9839d9813e8b3afc23a4
PaoloLoCasale <74263410+PaoloLoCasale@users.noreply.github.com>
2021-02-04 10:15:23 +0100

    Update esclassroom1,0.py

--- esclassroom1,0.py
+++ esclassroom1,0.py
@@ -1,23 +1,75 @@
 #Programma che preso un dizionario di studenti e voti restituisca un altro dizionario con studenti e medie valori
 
 import math
+print("I voti vanno da 18 a 30.")
 
-def main():
-    
-    dizionario_studenti = {"Marco":[24,28,25], "Fabio":[18,22,26],
-    "Alessandro":[28,23,27]}
-    print("Ecco i nomi degli studenti e l' elenco dei loro voti",dizionario_studenti)
-    
-    voti_marco = dizionario_studenti.get("Marco")
-    media_voti_marco = ((sum(voti_marco))/3)
-    voti_fabio = dizionario_studenti.get("Fabio")
-    media_voti_fabio = ((sum(voti_fabio))/3)
-    voti_ale = dizionario_studenti.get("Alessandro")
-    media_voti_ale = ((sum(voti_ale))/3)
-    
-    diz_voti = {"Marco": media_voti_marco, "Fabio": media_voti_fabio, "Alessandro": media_voti_ale}
-    print("Ecco la media dei voti per ogni studente",diz_voti)
+l_nomi = []
+l_media_voti = []
 
-main()
+diz = {}
+
+k1 = range(18, 23)
+k2 = range(24, 26)
+k3 = range(27,30)
+
+while True:
+    nome = input("Inserire i nomi degli studenti, digitare stop per smettere:\n")
+    nome = nome.capitalize()
+
+    if nome == "Stop":
+        break
+    l_nomi.append(nome)
+
+    media_voto = 0
+    l_voti = []
+
+    print("Inserire voti dello studente. Premere 0 per smettere:")
+
+    while True:
+        voto = int(input())
+        if voto == 0:
+            break
+        l_voti.append(voto)
+
+    for i in range(len(l_voti)):
+        media_voto += l_voti[i]
+
+    media_voto /= len(l_voti)
+    
+    lode = input("Lo studente ha una lode? ")
+    lode = lode.upper()
+    if lode == "SI":
+        media_voto = round(media_voto, 0)
+    else:
+        media_voto = math.trunc(media_voto)
+    
+    l_media_voti.append(media_voto)
+
+    for e in range(len(l_nomi)):
+        diz[l_nomi[e]] = l_media_voti[e]
+    
+
+def dizionario(l1 = [], l2 = [], l3 = []):
+
+    d_finale = {
+        k1 : l1,
+        k2 : l2,
+        k3 : l3
+    }
+    
+    for i in range(len(l_nomi)):
+        if l_media_voti[i] >= 18 and l_media_voti[i] <= 23:
+            l1.append(l_nomi[i])
+        elif l_media_voti[i] >= 24 and l_media_voti[i] <= 26:
+            l2.append(l_nomi[i])
+        elif l_media_voti[i] >= 27 and l_media_voti[i] <= 30:
+            l3.append(l_nomi[i])
+        else:
+            print(media_voto)
+            print("Media voti di", l_nomi[i], "non compatibile con i range.")
+    return d_finale
+
+d = dizionario()
+print(d)
     
 
